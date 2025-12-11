9aadb80871e6dceb85a2ba09f7daaa1fa0f92da7
Aezi <aezi.zhu@icloud.com>
2025-12-11 12:19:13 +0800

    chore: Update Homebrew formula for v1.5.0

--- Formula/chargetop.rb
+++ Formula/chargetop.rb
@@ -1,15 +1,16 @@
-class BatteryMon < Formula
+class Chargetop < Formula
   desc "Real-time Battery Monitor TUI for macOS"
   homepage "https://github.com/aezizhu/battery-mon"
-  url "https://github.com/aezizhu/battery-mon/archive/refs/heads/main.tar.gz"
-  version "1.0.0"
+  url "https://github.com/aezizhu/chargetop/archive/refs/tags/v1.5.0.tar.gz"
+  version "1.5.0"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000" # Placeholder, update after release
   license "MIT"
+  head "https://github.com/aezizhu/chargetop.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "-o", bin/"battery-mon"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "-o", bin/"chargetop"
   end
 
   test do
